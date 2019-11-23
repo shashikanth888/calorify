a76ac6401da52f1a47e1eaa7374e519095227080
billwu99 <billwu95@gmail.com>
2019-11-23 02:14:19 -0500

    vision detection

--- controllers/firebase_cloud.js
+++ controllers/firebase_cloud.js
@@ -1,11 +1,23 @@
-var bodyparser = require('body-parser');
+var bodyparser = require("body-parser");
+const vision = require("@google-cloud/vision");
 
-module.exports = function(app){
+const client = new vision.ImageAnnotatorClient();
 
-    app.use(bodyparser.json());
-    app.use(bodyparser.urlencoded({extended:true}));
+module.exports = function(app) {
+  app.use(bodyparser.json());
+  app.use(bodyparser.urlencoded({ extended: true }));
 
-    app.post('/api/imageurl', function(req,res){
-        url = req.body.url;
-    })
+  app.post("/api/imageurl", function(req, res) {
+    url = req.body.url;
+    detect_food(url);
+    res.send("received");
+  });
+};
+
+async function detect_food(url) {
+  // Performs label detection on the image file
+  const [result] = await client.labelDetection(url);
+  const labels = result.labelAnnotations;
+  console.log("Labels:");
+  labels.forEach(label => console.log(label.description));
 }
