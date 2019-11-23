39ba03d9798ea8c521c292aba316bd3b811a68ca
Ragulan Kodeeswaran <ragulan.kodeeswaran@gmail.com>
2019-11-23 18:26:04 -0500

    Updated front-end, finished Landing Page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from "react";
 import "./App.css";
 import LandingPage from "./components/LandingPage";
+import axios from "axios";
 
 class App extends Component {
   componentDidMount() {
-    let userDataForm = document.getElementById("user-from");
+    // let userDataForm = document.getElementById("user-from");
+    axios.get(`http://172.30.181.168:3000/api/test`).then(res => {
+      console.log(res.data.name);
+    });
 
-    fetch("http://172.30.181.168:3000/api/test")
-      .then(response => {
-        console.log(response.json());
-      })
-      .then(json => {
-        this.setState({
-          user: json
-        });
-      });
+    // fetch("http://172.30.181.168:3000/api/test")
+    //   .then(response => {
+    //     console.log(response.json());
+    //   })
+    //   .then(json => {
+    //     this.setState({
+    //       user: json
+    //     });
+    //   });
   }
 
   render() {
     return (
       <div>
         <LandingPage />
-        <div id="user-information"></div>
       </div>
     );
   }
--- front-end/src/components/LandingPage.js
+++ front-end/src/components/LandingPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Button from "react-bootstrap/Button";
 import Typed from "react-typed";
 // import Container from "react-bootstrap/Container";
@@ -12,53 +12,78 @@ import Typed from "react-typed";
 export default function LandingPage() {
   return (
     <header>
-      {/* NavBar */}
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand style={{ textTransform: "Uppercase" }} href="#home">
-          Calorify
-        </Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">1</Nav.Link>
-          <Nav.Link href="#features">2</Nav.Link>
-          <Nav.Link href="#pricing">3</Nav.Link>
-        </Nav>
-      </Navbar>
+      <div style={{ display: "flex" }}>
+        {/* NavBar */}
+        <div style={{ flex: 1 }} id="navbar">
+          <Navbar fixed="top" bg="dark" variant="dark">
+            <Navbar.Brand style={{ textTransform: "Uppercase" }} href="#home">
+              Calorify
+            </Navbar.Brand>
+
+            <div id="main-button-group" className="d-flex flex-column">
+              <ButtonGroup styel={{ margin: "auto" }} size="sm">
+                <Button>Calculate Calories</Button>
+                <Button>Know Your Nutrition History</Button>
+                <Button>Food Facts</Button>
+              </ButtonGroup>
+            </div>
+
+            {/* <div
+              style={{ width: "100%" }}
+              id="main-button-group"
+              className="container mr-auto"
+            >
+              <button>Calculate Calories</button>
+              <button>Know Your Nutrition History</button>
+              <button>Food Facts</button>
+            </div> */}
+            {/* <Nav className="mr-auto">
+              <Nav.Link classname="btn" href="#">
+                Calculate Calories
+              </Nav.Link>
+              <Nav.Link href="#">Know Your Nutrition History</Nav.Link>
+              <Nav.Link href="#">Food Facts</Nav.Link>
+            </Nav> */}
+          </Navbar>
+        </div>
+      </div>
 
       {/* Banner */}
       <section id="section-a">
-        <div className="container-fluid">
-          <div className="typed_banner">
-            <Typed
-              style={{
-                fontFamily: "'Tomorrow', sans-serif",
-                fontSize: "60px",
-                fontWeight: "100"
-              }}
-              strings={["Calorify..."]}
-              typeSpeed={50}
-              showCursor={false}
-            />
-            <br />
-            <Typed
-              style={{
-                fontFamily: "'Tomorrow', sans-serif",
-                fontSize: "30px"
-              }}
-              strings={["Estimate the food on your plate."]}
-              typeSpeed={50}
-              startDelay={800}
-              showCursor={true}
-            />
+        <div id="section-a-innertext" className="container-fluid">
+          <Typed
+            style={{
+              fontFamily: "'Tomorrow', sans-serif",
+              fontSize: "110px",
+              fontWeight: "100"
+            }}
+            strings={["Calorify"]}
+            typeSpeed={50}
+            showCursor={false}
+          />
+          <br />
+          <Typed
+            style={{
+              fontFamily: "'Tomorrow', sans-serif",
+              fontSize: "60px"
+            }}
+            strings={["Estimate the food on your plate."]}
+            typeSpeed={50}
+            startDelay={800}
+            showCursor={true}
+          />
+
+          <div id="headline">
+            <p style={{ fontSize: "30px", fontFamily: "'Calistoga', cursive" }}>
+              {" "}
+              You Don't Need To Be <strong>Perfect</strong>. Just{" "}
+              <strong>BE BETTER</strong> Than You Were Yesterday{" "}
+              <h1>
+                This Starts With Your <strong>Health!</strong>
+              </h1>
+              .
+            </p>
           </div>
-          <br />
-          <br />
-          <p>
-            This is a simple hero unit, a simple jumbotron-style component for
-            calling extra attention to featured content or information.
-          </p>
-          <p>
-            <Button variant="primary">Learn more</Button>
-          </p>
         </div>
       </section>
     </header>
