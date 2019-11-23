95fe0cf4e1a74a15864af754c4ea7b72ea986f2b
billwu99 <billwu95@gmail.com>
2019-11-23 12:35:23 -0500

    object recog

---
+++ detect.py
@@ -0,0 +1,62 @@
+import cv2 as cv
+import numpy as np
+import argparse
+import random as rng
+import math
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+rng.seed(12345)
+
+def thresh_callback(val, src_gray):
+    threshold = val
+    
+    canny_output = cv.Canny(src_gray, threshold, threshold * 2)
+    
+    contours, _ = cv.findContours(canny_output, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    maxarea = max([ cv.contourArea(i) for i in contours ])
+    maxcontour = None
+
+    contours_poly = None
+    boundRect = None
+    centers = None
+    radius = None
+    for i, c in enumerate(contours):
+        if cv.contourArea(c) == maxarea:
+            maxcontour = c
+            contours_poly = cv.approxPolyDP(c, 3, True)
+            boundRect = cv.boundingRect(contours_poly)
+            centers, radius = cv.minEnclosingCircle(contours_poly)
+    
+    drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
+    
+    color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
+    cv.rectangle(drawing, (int(boundRect[0]), int(boundRect[1])), \
+        (int(boundRect[0]+boundRect[2]), int(boundRect[1]+boundRect[3])), color, 2)
+    cv.circle(drawing, (int(centers[0]), int(centers[1])), int(radius), color, 2)
+    cv.imshow('Contours', drawing)
+    return radius, boundRect
+
+@app.route('/')
+def size():
+    url = request.args.get('url')
+    src = cv.imread(cv.samples.findFile('1.jpg'))
+    if src is None:
+        print('Could not open or find the image:', args.input)
+        return
+    src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
+    src_gray = cv.blur(src_gray, (3,3))
+    ret, src_gray = cv.threshold(src_gray, 127, 255, cv.THRESH_TOZERO)
+
+    source_window = 'Source'
+    cv.namedWindow(source_window)
+    cv.imshow(source_window, src)
+    max_thresh = 255
+    thresh = 100
+    cv.createTrackbar('Canny thresh:', source_window, thresh, max_thresh, thresh_callback)
+    radius, rect = thresh_callback(thresh, src_gray)
+    cv.waitKey()
+    return jsonify(area = math.pi*(radius*2.54/96)**2)
+
+if __name__ == '__main_':
+    app.run(debug=True, port=5000) #run app in debug mode on port 5000
--- server/app.js
+++ server/app.js
@@ -1,9 +1,50 @@
-var express = require('express');
+var express = require("express");
+var bodyparser = require("body-parser");
 var app = express();
+const request = require("request");
+const Clarifai = require("clarifai");
 
-var firebase_controller = require('./controllers/firebase_cloud');
+const model = new Clarifai.App({ apiKey: "1f9bb490007547d4a0070b895e9487e2" });
+
 var port = process.env.PORT || 3000;
 
-firebase_controller(app);
+// var firebase_controller = require("./controllers/firebase_cloud");
+// firebase_controller(app);
 
-app.listen(port);
+const setup = () => {
+  app.use(bodyparser.urlencoded({ extended: false }));
+  app.use(bodyparser.json());
+  app.get("/", (req, res) => res.send("Hello World!"));
+  app.get("/api/imageurl", function(req, res) {
+    url = req.query.url;
+    detect(url);
+    res.send(url);
+  });
+  app.post("/api/base64", (req, res) => {
+    image = req.body.img;
+  });
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+};
+
+detect = url => {
+  model.models
+    .predict("bd367be194cf45149e75f01d59f77ba7", url)
+    .then(response => {
+      out = {
+        name: response.outputs[0].data.concepts[0].name,
+        confidence: response.outputs[0].data.concepts[0].value
+      };
+      return out, address;
+    })
+    .then(make_req);
+};
+
+make_req = (out, address) =>
+  request(address, { json: true }, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log(body);
+  });
+
+setup();
